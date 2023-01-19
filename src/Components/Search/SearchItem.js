35372e674fe1771e985323c480e505106d62ee36
rknicker <rknicker88@gmail.com>
2023-01-18 21:29:10 -0700

    Update SearchItem.js

--- src/Components/Search/SearchItem.js
+++ src/Components/Search/SearchItem.js
@@ -6,14 +6,15 @@ export default function SearchItem({ item }) {
     <Link href={`/${item?.slug}`} className="DataItem">
       <div className="nameVersion">
         <h3 className="ItemName">{item?.name}</h3>
-        <p className="ItemVersion">{item?.wordVersion}</p>
+        /* <p className="ItemVersion">{item?.wordVersion}</p> */ 
+        <h3 className="ItemVersion">{item?.wordVersion}</h3>
       </div>
       <div className="SearchItemDetay">
         <span className="ItemDesc">{item?.description?.slice(0, 50)} </span>
         ...
         <p className="showMore" rel="noreferrer" href={`/${item?.slug}`}>
-          // {" "}
-          // More
+          /*{" "}*/
+          more
         </p>
       </div>
     </Link>

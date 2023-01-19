a2436db42861bccf682e708e18642799c5a6938d
rknicker <rknicker88@gmail.com>
2023-01-18 21:41:29 -0700

    Update SearchItem.js
    changed <Link href={`/${item?.slug}`} .. to <div

--- src/Components/Search/SearchItem.js
+++ src/Components/Search/SearchItem.js
@@ -3,20 +3,18 @@ import React from "react";
 
 export default function SearchItem({ item }) {
   return (
-    <Link href={`/${item?.slug}`} className="DataItem">
-      <div className="nameVersion">
+    <div className="DataItem">
+      <div className="nameVersion" href={`/${item?.slug}`}>
         <h3 className="ItemName">{item?.name}</h3>
-        /* <p className="ItemVersion">{item?.wordVersion}</p> */ 
         <h3 className="ItemVersion">{item?.wordVersion}</h3>
       </div>
       <div className="SearchItemDetay">
-        <span className="ItemDesc">{item?.description?.slice(0, 50)} </span>
+        <span className="ItemDesc" href={`/${item?.slug}`}> {item?.description?.slice(0, 50)} </span>
         ...
         <p className="showMore" rel="noreferrer" href={`/${item?.slug}`}>
-          /*{" "}*/
           more
         </p>
       </div>
-    </Link>
+    </div>
   );
 }

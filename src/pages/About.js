e4921ac1f14e20df7389297a488bb2f0cce4a243
rknicker <rknicker88@gmail.com>
2023-01-26 13:10:24 -0700

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,6 +12,37 @@ export default function About() {
         <div className="AboutDesc">
           <h1>About</h1>
           <p>
+            If you are here because you cant find a specific word or term in the Open 
+            Dictionary, consider following these steps. 
+          </p>
+    
+          <ul>
+            <li>
+            Find the most common definition from a collection of contemporary dictionaries.
+            </li>
+            <li>
+            In an example, we take 11 English dictionaries, some are printed volumes, others are online repositories.
+            </li>
+            <li>
+            Of the 11 dictionaries, all of them have a definition for the word "apple". 2 of them have multiple definitions.
+            </li>
+            <li>
+            Of the 13 total definitions, 11 describe a colored fruit with white flesh with seeds in it's star shaped core,
+            and 2 describe a brand of consumer electronics related to iPhone iMac, Macbook, AppleTV, etc.
+            </li>
+            <li>
+            The serial number for the more common definitions would be 1.1.1, where the serial number for the second most 
+            common definition would be 1.1.2. 
+            </li>
+            <li>
+            One can use the Open Dictionary notation for words that are not yet included in 
+            this database by performing the above activity, and then writing "Apple 1.1.2" in the body of the writing, and in a footnote
+            writing "Open Dictionary v1" or "ODv1". to learn about the proper serial number syntax, please search this database for 
+            "Open Dictionary 2.1.1" like you would search for any other word. 
+            </li>
+          </ul>
+    
+          <p>
             The Open Dictionary assigns a serial number for each meaning 
             that a word or phrase needs to be used for. Some words have 
             multiple definitions in a dictionary, and context sensing is 

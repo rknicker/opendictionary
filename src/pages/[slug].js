23e58ea2a4436c8aa39c4d9cca8cb7dd3d091ce9
rknicker <rknicker88@gmail.com>
2023-01-26 11:59:19 -0700

    Update [slug].js

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -17,7 +17,7 @@ export const generatedData = DictionaryData.map((item) => ({
   md5: MD5(item?.description).toString(),
 }))
 
-const Details = ({ item }) => {
+const Details = ({ data }) => {
   const effectRan = useRef(false)
   const { asPath } = useRouter()
   const origin =
@@ -34,9 +34,10 @@ const Details = ({ item }) => {
       script.src = 'https://telegram.org/js/telegram-widget.js?21'
       script.setAttribute(
         'data-telegram-discussion',
-        `${telegramChannelName}/${item?.telegramPost}`
+        `${telegramChannelName}/${data?.telegramPost}`
       )
       script.setAttribute('data-comments-limit', '5')
+      script.setAttribute('data-color', '071120')
       document.getElementById('comments-container').appendChild(script)
     }
     return () => (effectRan.current = true)
@@ -44,26 +45,26 @@ const Details = ({ item }) => {
   return (
     <>
       <Head>
-        <title>{item?.name} - #OpenDictionary</title>
+        <title>{data?.name} - #OpenDictionary</title>
         <link rel='canonical' href={URL} />
       </Head>
       <div className='Detay'>
-        <h1 className='DetayHead'>{item?.name}</h1>
-        <p className='DetayVersion'>{item?.wordVersion}</p>
+        <h1 className='DetayHead'>{data?.name}</h1>
+        <p className='DetayVersion'>{data?.wordVersion}</p>
       </div>
 
-      <div>
-        <p>{item?.description}</p>
+      <div className='Detay'>
+        <p>{data?.description}</p>
       </div>
 
       <div className='DetayMd5'>
-        <span>{item?.md5}</span>
+        <span>{data?.md5}</span>
       </div>
 
       <div className='DetayIconBar'>
         <IconsBar
-          likeLink={`${likeButtonLink}${item?.telegramPost}`}
-          saveLink={`${Telegram}/${item?.telegramPost}`}
+          likeLink={`${likeButtonLink}${data?.telegramPost}`}
+          saveLink={`${Telegram}/${data?.telegramPost}`}
           whiteBackground
         />
       </div>
@@ -74,7 +75,7 @@ const Details = ({ item }) => {
 
 export async function getStaticPaths() {
   return {
-    paths: item.map((item) => ({
+    paths: generatedData.map((item) => ({
       params: {
         slug: item.slug,
       },
@@ -86,7 +87,7 @@ export async function getStaticPaths() {
 export async function getStaticProps(context) {
   return {
     props: {
-      data: item.filter(
+      data: generatedData.filter(
         (item) => item.slug === context.params?.slug
       )?.[0],
     },

de5385530608d2109c4b5f95a6a79f1d765263e7
rknicker <rknicker88@gmail.com>
2023-01-26 14:37:31 -0700

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -41,7 +41,7 @@ export default function About() {
           <p>
             One can use the Open Dictionary notation for words that are not yet included in 
             this database by performing the above activity, and then writing &quot;Apple 1.1.2&quot; in the body of the writing, and in a footnote
-            writing &quot;Open Dictionary 2.1.1&quot; or &quot;&#35;OD1&quot;. to learn about the proper serial number syntax, please search this database for 
+            writing &quot;Open Dictionary 2.1.1&quot; or &quot;&#35;OD1&quot;. To learn about the proper serial number syntax, please search this database for 
             &quot;Open Dictionary 2.1.1&quot; like you would search for any other word. 
           </p>
           <br />

6dd925b572bf02806f07ed796cafe861d5d58cea
rknicker <rknicker88@gmail.com>
2023-01-26 12:17:23 -0700

    Update SearchItem.js

--- src/Components/Search/SearchItem.js
+++ src/Components/Search/SearchItem.js
@@ -9,10 +9,11 @@ export default function SearchItem({ item }) {
         <h3 className="ItemVersion">{item?.wordVersion}</h3>
       </div>
       <div className="SearchItemDetay">
-        <span className="ItemDesc">{item?.description?.slice(0, 50)}</span>
-        <p className="showMore" rel="noreferrer">
-           . . . more
-        </p>
+        <span className="ItemDesc">{item?.description?.slice(0, 50)}
+          <p className="showMore" rel="noreferrer">
+             . . . more
+          </p>
+        </span>
       </div>
     </Link>
   );

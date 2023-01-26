413751a26ee84b27c50d6173d9fd85a9ff569cf0
rknicker <rknicker88@gmail.com>
2023-01-26 13:21:26 -0700

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -15,33 +15,35 @@ export default function About() {
             If you are here because you cant find a specific word or term in the Open 
             Dictionary, consider following these steps. 
           </p>
-    
+          <br />
+          <p>
+            Find the most common definition from a collection of contemporary dictionaries.
+          </p>
+          <br />
           <ul>
             <li>
-            Find the most common definition from a collection of contemporary dictionaries.
+              In an example, we take 11 English dictionaries, some are printed volumes, others are online repositories.
             </li>
             <li>
-            In an example, we take 11 English dictionaries, some are printed volumes, others are online repositories.
+              Of the 11 dictionaries, all of them have a definition for the word "apple". 2 of them have multiple definitions.
             </li>
             <li>
-            Of the 11 dictionaries, all of them have a definition for the word "apple". 2 of them have multiple definitions.
+              Of the 13 total definitions, 11 describe a colored fruit with white flesh with seeds in it's star shaped core,
+              and 2 describe a brand of consumer electronics related to iPhone iMac, Macbook, AppleTV, etc.
             </li>
             <li>
-            Of the 13 total definitions, 11 describe a colored fruit with white flesh with seeds in it's star shaped core,
-            and 2 describe a brand of consumer electronics related to iPhone iMac, Macbook, AppleTV, etc.
+              The serial number for the more common definitions would be 1.1.1, where the serial number for the second most 
+              common definition would be 1.1.2. 
             </li>
-            <li>
-            The serial number for the more common definitions would be 1.1.1, where the serial number for the second most 
-            common definition would be 1.1.2. 
-            </li>
-            <li>
+          </ul>
+          <br />
+          <p>
             One can use the Open Dictionary notation for words that are not yet included in 
             this database by performing the above activity, and then writing "Apple 1.1.2" in the body of the writing, and in a footnote
             writing "Open Dictionary v1" or "ODv1". to learn about the proper serial number syntax, please search this database for 
             "Open Dictionary 2.1.1" like you would search for any other word. 
-            </li>
-          </ul>
-    
+          </p>
+          <br />
           <p>
             The Open Dictionary assigns a serial number for each meaning 
             that a word or phrase needs to be used for. Some words have 

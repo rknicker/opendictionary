7c37e882239ad03d728061dfd88f4037118fdc6f
rknicker <rknicker88@gmail.com>
2023-01-26 12:07:54 -0700

    Update constants.js

--- src/Data/constants.js
+++ src/Data/constants.js
@@ -1,1 +1,19 @@
-export const telegramChannelName = "opendictionary1";
+export const telegramChannelName = 'opendictionary1'
+// For you Navbar in Links Dynamic Changed
+export const createWordButtonLink = 'https://t.me/opendictionary1/4`'
+export const ContributeLink =
+  'https://github.com/ademsuslu/Dictionary/blob/main/src/Data/dictionary.js'
+export const HomePage = '/'
+export const AboutPage = '/About'
+export const Telegram = `https://t.me/${telegramChannelName}`
+//Navbar End
+
+// Toast Error Dynamic Message
+export const NoResultMessage = `No results. Please check spelling and read About Us. Happy Searching!`
+// Toast Error Dynamic Message  End
+
+export const likeButtonLink = 'tg://resolve?domain=opendictionary1&post='
+
+
+/*This is the telegram group name for the dictionary */
+/*This is the link connected to the plus sign on the home page, for adding a word */

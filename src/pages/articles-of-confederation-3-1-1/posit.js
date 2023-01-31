733cde3449f06668485d6de32238a26e3d29ee66
rknicker <rknicker88@gmail.com>
2023-01-31 10:49:41 -0700

    Create posit.js

---
+++ src/pages/articles-of-confederation-3-1-1/posit.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Posit2() {
+  return (
+    <>
+      <Head>
+        <title>posit1</title>
+      </Head>
+
+      <div className="About">
+        <div className="AboutDesc">
+          <h1>posit</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores
+            fugit in quaerat eum, ratione at laborum iure quo odio numquam
+            magnam pariatur, deleniti quos ad reiciendis tempora libero,
+            excepturi amet.
+          </p>{" "}
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores
+            fugit in quaerat eum, ratione at laborum iure quo odio numquam
+            magnam pariatur, deleniti quos ad reiciendis tempora libero,
+            excepturi amet.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}

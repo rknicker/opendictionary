890e6ab5a60d97e37611f278aabf0947b8837be5
rknicker <rknicker88@gmail.com>
2023-02-04 20:13:19 -0700

    Update dictionary.js

--- src/Data/dictionary.js
+++ src/Data/dictionary.js
@@ -26,9 +26,27 @@ const dictionary = [
     version: "1.2",
   },
   {
+    name: "Réx",
+    wordVersion: "1.1.1",
+    slug: "Réx-1-1-1",
+    description:
+      "Written with latin characters from the UTF-8 character set (82 233 120). Used for a one way label from the prp to revere honor and respect the mdp existence from whence the breath of life transmits. The metaphors: driver of the car, spirit of the body, operator of the crane, electricity and programming of the mechatronics device. The body portion of the living soul remains with the prp after death. The spirit remains with the mdp. During the time between conception and death, the body and the spirit are in close communication. The spirit is free to leave, to travel, and to experience other worlds during the time between conception and death only if the higher self allows and the spirit chooses. Ninety-eight percent of the between conception and death, Réx operates a body that is built with John and Gail combined prp energy. See Rex Knickerbocker 2.1.1",
+    telegramPost: "18",
+    version: "1.0",
+  },
+  {
+    name: "Réxated",
+    wordVersion: "1.1.1",
+    slug: "Réxated-1-1-1",
+    description:
+      "Written with latin characters from the UTF-8 character set (82 233 120 97 116 101 100). The living soul property of Réx 1.1.1 as described in Genesis 2:7. Réx and Ated 1.1.2 when Réx is in close proximity to Ated and when Réx is controlling Ated. Ated is born of Gail and John. Ated formed over many years starting in the gregorian year 1987, in an area called Eugene Oregon. Gail and John named Réxated by custom, desire, love, and honor, the name Rex Arthur Knickerbocker. Furthermore, Gail and John followed local customs and signed items for birth record registration. Ated was preceded by Nalihda, Joseph, and Jean-Michel. See Réx 1.1.1, Rex Knickerbocker 2.1.1",
+    telegramPost: "19",
+    version: "1.0",
+  },
+  {
     name: "United States",
     wordVersion: "2.1.1",
-    slug: "united-states-2-1-1",
+    slug: "United-States-2-1-1",
     description:
       "A Nation that borders with Canada to the north and Mexico to the south, with coasts on the Atlantic and the Pacific.",
     telegramPost: "8",

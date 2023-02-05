f04c86d879386592ec065a36cfe9223fa84d40c1
rknicker <rknicker88@gmail.com>
2023-02-04 21:05:41 -0700

    Update dictionary.js

--- src/Data/dictionary.js
+++ src/Data/dictionary.js
@@ -2,7 +2,7 @@ const dictionary = [
   {
     name: "Apple",
     wordVersion: "1.1.1",
-    slug: "Apple-1-1-1",
+    slug: "-apple-1-1-1",
     description:
       "A round fruit with red or green skin and a firm white inside.",
     telegramPost: "5",
@@ -11,7 +11,7 @@ const dictionary = [
   {
     name: "Apple",
     wordVersion: "1.1.2",
-    slug: "Apple-1-1-2",
+    slug: "-apple-1-1-2",
     description:
       "Maker of iPhone, Macbook, Airpods, Apple Watch, Apple TV, etc.",
     telegramPost: "23",
@@ -20,7 +20,7 @@ const dictionary = [
   {
     name: "Articles of Confederation",
     wordVersion: "3.1.1",
-    slug: "Articles-of-Confederation-3-1-1",
+    slug: "-articles-of--confederation-3-1-1",
     description: "A document that was a product of the second continental congress of the united states of America. The document was written by the congress in 1776-1777.",
     telegramPost: "15",
     version: "1.2",
@@ -37,7 +37,7 @@ const dictionary = [
   {
     name: "Community DAO",
     wordVersion: "2.1.1",
-    slug: "Community-DAO-2-1-1",
+    slug: "-community--d-a-o-2-1-1",
     description:
       "The Community DAO 2.1.1 (Distributed Autonomous Organization) is the secular arm of the We Church 2.1.1. Community DAO is a foundation vested with the intent of the god that living souls build a heaven of good diversity with respect to emotional interactions with others and self reliance physically. Harmony need not require supply chains. Commerce and slavery have long been used to fulfill basic needs, as an example most water is piped in, most electricity is taken from the electrical grid, and most toilet paper is manufactured offsite. Without knowing by whom and in what circumstances the commodities are sourced, the probability of slavery is unknown and therefore forgettable. Community DAO is focused on a higher mission, but this heaven to be will need to exclude involuntary servitude on its face.",
     telegramPost: "21",
@@ -46,7 +46,7 @@ const dictionary = [
   {
     name: "Declaration of Independence",
     wordVersion: "3.1.1",
-    slug: "Declaration-of-Independence-3-1-1",
+    slug: "-declaration-of--independence-3-1-1",
     description:
       "The document created and approved 4 July 1776 as the organic document to form the United States of America.",
     telegramPost: "22",
@@ -55,7 +55,7 @@ const dictionary = [
   {
     name: "Réx",
     wordVersion: "1.1.1",
-    slug: "Réx-1-1-1",
+    slug: "-réx-1-1-1",
     description:
       "Written with latin characters from the UTF-8 character set (82 233 120). Used for a one way label from the prp to revere honor and respect the mdp existence from whence the breath of life transmits. The metaphors: driver of the car, spirit of the body, operator of the crane, electricity and programming of the mechatronics device. The body portion of the living soul remains with the prp after death. The spirit remains with the mdp. During the time between conception and death, the body and the spirit are in close communication. The spirit is free to leave, to travel, and to experience other worlds during the time between conception and death only if the higher self allows and the spirit chooses. Ninety-eight percent of the between conception and death, Réx operates a body that is built with John and Gail combined prp energy. See Rex Knickerbocker 2.1.1",
     telegramPost: "18",
@@ -64,7 +64,7 @@ const dictionary = [
   {
     name: "Réxated",
     wordVersion: "1.1.1",
-    slug: "Réxated-1-1-1",
+    slug: "-réxated-1-1-1",
     description:
       "Written with latin characters from the UTF-8 character set (82 233 120 97 116 101 100). The living soul property of Réx 1.1.1 as described in Genesis 2:7. Réx and Ated 1.1.2 when Réx is in close proximity to Ated and when Réx is controlling Ated. Ated is born of Gail and John. Ated formed over many years starting in the gregorian year 1987, in an area called Eugene Oregon. Gail and John named Réxated by custom, desire, love, and honor, the name Rex Arthur Knickerbocker. Furthermore, Gail and John followed local customs and signed items for birth record registration. Ated was preceded by Nalihda, Joseph, and Jean-Michel. See Réx 1.1.1, Rex Knickerbocker 2.1.1",
     telegramPost: "19",
@@ -73,7 +73,7 @@ const dictionary = [
   {
     name: "United States",
     wordVersion: "2.1.1",
-    slug: "United-States-2-1-1",
+    slug: "-united--states-2-1-1",
     description:
       "A Nation that borders with Canada to the north and Mexico to the south, with coasts on the Atlantic and the Pacific.",
     telegramPost: "8",
